27ed0d0a7970e3660e315ae508f5995045948b8b
anhquangmobile <quangduong@tel4vn.com>
2023-04-13 16:00:47 +0700

    feat: firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:pitel_ui_kit/app.dart';
+import 'package:plugin_pitel/voip_push/firebase_options.dart';
 import 'package:plugin_pitel/voip_push/push_notif.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await PushNotifAndroid.initFirebase();
+  await PushNotifAndroid.initFirebase(DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     const ProviderScope(
