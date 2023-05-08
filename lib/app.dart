b287d3445600cc675c990f7d67b29fcfb355f3c8
anhquangmobile <quangduong@tel4vn.com>
2023-05-08 14:10:17 +0700

    feat: support test

--- lib/app.dart
+++ lib/app.dart
@@ -6,22 +6,37 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pitel_ui_kit/routing/app_router.dart';
 import 'package:plugin_pitel/flutter_pitel_voip.dart';
 
+// final sipInfoData = SipInfoData.fromJson({
+//   "authPass": "${Password}",
+//   "registerServer": "${Domain}",
+//   "outboundServer": "${Outbound Proxy}",
+//   "userID": UUser,                // Example 101
+//   "authID": UUser,                // Example 101
+//   "accountName": "${UUser}",      // Example 101
+//   "displayName": "${UUser}@${Domain}",
+//   "dialPlan": null,
+//   "randomPort": null,
+//   "voicemail": null,
+//   "wssUrl": "${URL WSS}",
+//   "userName": "${username}@${Domain}",
+//   "apiDomain": "${URL API}"
+// });
+
 final sipInfoData = SipInfoData.fromJson({
-  "authPass": "${Password}",
-  "registerServer": "${Domain}",
-  "outboundServer": "${Outbound Proxy}",
-  "userID": UUser,                // Example 101
-  "authID": UUser,                // Example 101
-  "accountName": "${UUser}",      // Example 101
-  "displayName": "${UUser}@${Domain}",
+  "authPass": "Tel4vn.com123@",
+  "registerServer": "mobile.tel4vn.com",
+  "outboundServer": "pbx-mobile.tel4vn.com:50061",
+  "userID": 103,
+  "authID": 103,
+  "accountName": "103",
+  "displayName": "103@mobile.tel4vn.com",
   "dialPlan": null,
   "randomPort": null,
   "voicemail": null,
-  "wssUrl": "${URL WSS}",
-  "userName": "${username}@${Domain}",
-  "apiDomain": "${URL API}"
+  "wssUrl": "wss://wss-mobile.tel4vn.com:7444",
+  "userName": "user3@mobile.tel4vn.com",
+  "apiDomain": "https://api-mobile.tel4vn.com"
 });
-
 
 const String deviceToken = '';
 
@@ -55,14 +70,21 @@ class _MyAppState extends State<MyApp> {
   void handleRegister() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     final deviceTokenRes = await PushVoipNotif.getDeviceToken();
-
     final pnPushParams = PnPushParams(
       pnProvider: Platform.isAndroid ? 'fcm' : 'apns',
       pnParam: Platform.isAndroid
-          ? '${bundleId}'                         // Example com.company.app
-          : '${apple_team_id}.${bundleId}.voip',  // Example com.company.app
+          ? packageInfo.packageName
+          : 'XP2BMU4626.${packageInfo.packageName}.voip',
       pnPrid: deviceTokenRes,
     );
+
+    // final pnPushParams = PnPushParams(
+    //   pnProvider: Platform.isAndroid ? 'fcm' : 'apns',
+    //   pnParam: Platform.isAndroid
+    //       ? '${bundleId}' // Example com.company.app
+    //       : '${apple_team_id}.${bundleId}.voip', // Example com.company.app
+    //   pnPrid: deviceTokenRes,
+    // );
 
     pitelService.setExtensionInfo(sipInfoData, pnPushParams);
   }
